dbd20e98b24be5f4da5b9d6b502f1ad1f25a70a4
ksh-mau <105494634+ksh-mau@users.noreply.github.com>
2023-11-20 22:46:11 -0500

    Transporter.java

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,3 +1,41 @@
 package edu.uc.cs3003.medava;
+
+import java.util.ArrayList;
+
 public class Transporter {
+    private ArrayList<Shippable > goods;
+    { goods = new ArrayList<Shippable >() {
+        
+    };}
+    public Transporter(String transporterName, double lowTemp, double highTemp) {
+        mTransporterName = transporterName;
+        mLowTemperature = lowTemp;
+        mHighTemperature = highTemp;
+    }
+    private String mTransporterName;
+
+    public String getTransporterName() {
+        return mTransporterName;
+    }
+
+
+    public void ship() {
+        
+    }
+
+    private double mLowTemperature, mHighTemperature;
+    
+    
+    public boolean load(Shippable itemToLoad) {
+        if (itemToLoad.isTemperatureRangeAcceptable(mLowTemperature, mHighTemperature)) {
+            return goods.add(itemToLoad);
+        }
+        return false;
+    }
+    public Shippable unload() {
+        return goods.remove(0);
+    }
+    public boolean isEmpty() {
+        return goods.isEmpty();
+    }
 }
